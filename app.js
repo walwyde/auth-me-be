38d2db85854db1f507a784b9aae1c85c9d31ebbf
walwyde <walwyde@gmail.com>
2025-02-17 14:27:51 +0100

    init commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
-app.use(body_parser({extended: false}))
+app.use(body_parser.urlencoded({extended: true}))
 
 console.log(environment)
 
